49f84b3e99732ad3fb0ac4785f5a0be54a40a789
Jane Usoskina <jusoskina@192.168.1.102>
2017-04-29 19:07:49 +0100

    Added another app

---
+++ howdy/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HowdyPageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'howdyindex.html', context=None)
